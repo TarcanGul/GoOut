20319b0f2e2cfcdb9f73cabde64f82d4adfd1546
Vaastav Arora <arora74@purdue.edu>
2019-04-15 20:09:18 -0400

    hello world

--- back-end/index.py
+++ back-end/index.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+import os
+APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH = os.path.join(APP_PATH,"../front-end")
+
 
 @app.route("/")
-def indexWebsite()
-{
-	return render_template("../front-end/index.html")
-}
+def indexWebsite():
+	return "Hello World!"
