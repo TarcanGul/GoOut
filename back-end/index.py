cf1b1c23d5326084fbef50a21f23427647735cac
nailt <nailtarcan@gmail.com>
2019-04-16 21:05:28 -0400

    CSS support added in static folder, auth.html finished

--- back-end/index.py
+++ back-end/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 app = Flask(__name__, template_folder="front-end")
 import os
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(APP_PATH,"../front-end")
-
+app.static_folder="static"
 
 @app.route("/")
 def indexWebsite():
@@ -14,3 +14,9 @@ def indexWebsite():
 @app.route("/createEvent")
 def serveCreateEventPage():
   return render_template("createEvent.html")
+
+@app.route("/auth")
+def serveAuthPage():
+  return render_template("auth.html")
+
+
