89110a7ff7411f51b4421221867608333839806a
nailt <nailtarcan@gmail.com>
2019-04-17 20:03:54 -0400

    Getting the username and password from signup form

--- back-end/index.py
+++ back-end/index.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from flask import render_template, url_for
-app = Flask(__name__, template_folder="front-end")
+from flask import render_template, url_for, request
 import os
+import sys
+import hashlib
+
+app = Flask(__name__, template_folder="front-end")
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(APP_PATH,"../front-end")
 app.static_folder="static"
@@ -17,6 +20,16 @@ def serveCreateEventPage():
 
 @app.route("/auth")
 def serveAuthPage():
-  return render_template("auth.html")
+  if request.method == 'GET':
+    return render_template("auth.html")
 
+@app.route("/handlesignup", methods=['POST'])
+def handleSignUp():
+  username = request.form['username']
+  #Hashing the password in MD5
+  raw_password = request.form['password']
+  raw_password.encode("utf-8")
+  password = hashlib.md5(raw_password.encode())
+  #TODO: Push username and password to the database. 
+  return "<h1>" + username + "</h1>\n" + "<h2>" + password.hexdigest() + "</h2>"
 
